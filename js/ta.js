389ac7a904f5df7648cb50acd6f4b4a8dedd6b4c
Wealth <30428646+Wea1thRS@users.noreply.github.com>
2021-08-02 19:52:23 -0500

    Changed some fonts, added a hidden slider, and other stuff.

--- js/ta.js
+++ js/ta.js
@@ -1,3 +1,26 @@
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
 // Array to store our Snowflake objects
 const snowflakes = [];
 
@@ -5,8 +28,23 @@ const snowflakes = [];
 let browserWidth;
 let browserHeight;
 
+const slider = document.getElementById("flak");
+
+console.log(slider);
+
+function updateSlider(slideAmount) {
+    setCookie("flakes", slideAmount, 365);
+}
+
+if (document.cookie.indexOf('flakes=') === -1) {
+    setCookie("flakes", 30, 365)
+} else {
+    flakes = parseInt(getCookie("flakes"));
+}
+
 // Specify the number of snowflakes you want visible
-const numberOfSnowflakes = 30;
+
+const numberOfSnowflakes = flakes;
 
 // Flag to reset the position of the snowflakes
 let resetPosition = false;
